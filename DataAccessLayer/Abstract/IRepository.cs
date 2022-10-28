ec0c76c7db45c378a69cd6a474cef41ae6e5ff62
EsraNurPalut <nuresrapalut@outlook.com>
2022-10-28 18:19:24 +0300

    TRepository interface oluşturduk.

---
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+   public interface IRepository<T>  //Hepsini tek tek ıcotegorydal gibi yazmaktansa hespni t ye tanımladık 
+    {
+        List<T> List();
+
+        void Insert(T p);
+
+        void Delete(T p);
+
+        void Update(T p);
+
+    }
+}
