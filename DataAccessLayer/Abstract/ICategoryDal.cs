5c91a71c660ce405dc73a04aa11268dfcb1e8aee
EsraNurPalut <nuresrapalut@outlook.com>
2022-10-28 18:01:10 +0300

    Abstract ve Interface Bileşenleri

---
+++ DataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer1.Concrete;
+
+namespace DataAccessLayer.Abstract
+{
+   public interface ICategoryDal
+    {
+        //CRUD
+        //Typer Name();--- Metot tanımlarken
+
+        List<Category> List(); //listeleme
+
+        void Insert(Category p);  //ekleme işlemi
+
+        void Update(Category p);  //güncelleme
+
+        void Delete(Category p); //silme 
+
+    }
+}
