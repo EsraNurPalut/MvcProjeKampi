8d04c99edad9da71223baacd65a78149fba6138b
EsraNurPalut <nuresrapalut@outlook.com>
2022-11-01 22:02:05 +0300

    WriterManager kısmı düzenlendi

---
+++ BusinessLayer1/Concrete/WriterManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer1.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer1.Concrete;
+
+namespace BusinessLayer1.Concrete
+{
+    public class WriterManager : IWriterService
+    {
+
+        IWriterDal _writerDal;
+
+        public WriterManager(IWriterDal writerDal)
+        {
+            _writerDal = writerDal;
+        }
+
+        public Writer GetByID(int id)
+        {
+            return _writerDal.Get(x => x.WriterID == id);
+        }
+
+        public List<Writer> GetList()
+        {
+            return _writerDal.List();
+        }
+
+        public void WriterAdd(Writer writer)
+        {
+            _writerDal.Insert(writer);
+        }
+
+        public void WriterDelete(Writer writer)
+        {
+            _writerDal.Delete(writer);
+        }
+
+        public void WriterUpdate(Writer writer)
+        {
+            _writerDal.Update(writer);
+        }
+    }
+}
