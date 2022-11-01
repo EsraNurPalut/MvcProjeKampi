65c34ff7b6b519ca185d4464de5f909dc542d3ea
EsraNurPalut <nuresrapalut@outlook.com>
2022-11-01 22:02:19 +0300

    Writer işlemleri düzenlendi

---
+++ MvcProjeKampi/Controllers/WriterController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer1.Concrete;
+using DataAccessLayer.EntityFramework;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class WriterController : Controller
+    {
+
+        WriterManager wm = new WriterManager(new EFWriterDal());
+        // GET: Writer
+        public ActionResult Index()
+        {
+            var WriterValues = wm.GetList();
+            return View(WriterValues);
+        }
+    }
+}
