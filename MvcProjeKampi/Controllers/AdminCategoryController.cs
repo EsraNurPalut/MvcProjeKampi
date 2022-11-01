7d91e81448b8f4a4b0db7a6357dd21f754b6ac3a
EsraNurPalut <nuresrapalut@outlook.com>
2022-11-01 11:55:20 +0300

    Tema düzenlendi

--- MvcProjeKampi/Controllers/AdminCategoryController.cs
+++ MvcProjeKampi/Controllers/AdminCategoryController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BusinessLayer1.Concrete;
+using DataAccessLayer.EntityFramework;
 
 namespace MvcProjeKampi.Controllers
 {
     public class AdminCategoryController : Controller
     {
-        // GET: AdminCategory
+        CategoryManager cm = new CategoryManager(new EFCategoryDal());
+
         public ActionResult Index()
         {
-            return View();
+            var categoryvalues = cm.GetList();
+            return View(categoryvalues);
         }
     }
 }
