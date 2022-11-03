a8565d7fe6d49f68b092a68504953abaaabcac7b
EsraNurPalut <nuresrapalut@outlook.com>
2022-11-03 23:00:59 +0300

    Heading işlemleri

---
+++ MvcProjeKampi/Controllers/HeadingController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer1.Concrete;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class HeadingController : Controller
+    {
+        // GET: Heading
+        HeadingManager hm = new HeadingManager(new EFHeadingDal());
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
