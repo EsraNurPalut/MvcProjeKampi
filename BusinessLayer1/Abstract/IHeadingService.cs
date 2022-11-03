d29931a8f74a83c7a7f7ea46f2077af8c2ba3a95
EsraNurPalut <nuresrapalut@outlook.com>
2022-11-03 22:35:35 +0300

    IHeadingService

---
+++ BusinessLayer1/Abstract/IHeadingService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer1.Concrete;
+
+namespace BusinessLayer1.Abstract
+{
+    public interface IHeadingService
+    {
+        List<Heading> GetList();
+        void HeadingAdd(Heading heading);
+        Heading GetByID(int id);
+        void HeadingDelete(Heading heading);
+        void HeadingUpdate(Heading heading)
+    }
+}
