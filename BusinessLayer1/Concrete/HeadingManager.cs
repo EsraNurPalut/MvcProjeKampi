389b0e5e98e39b2d496f4d1ed6dc18e0618df59d
EsraNurPalut <nuresrapalut@outlook.com>
2022-11-03 22:42:11 +0300

    HeadingManager düzenlendi

---
+++ BusinessLayer1/Concrete/HeadingManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer1.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer1.Concrete;
+
+namespace BusinessLayer1.Concrete
+{
+    public class HeadingManager : IHeadingService
+    {
+        IHeadingDal _HeadingDal;
+
+        public HeadingManager(IHeadingDal headingDal)
+        {
+            _HeadingDal = headingDal;
+        } //constr metodu
+
+        public Heading GetByID(int id)
+        {
+            return _HeadingDal.Get(x => x.HeadingID == id);
+        }
+
+        public List<Heading> GetList()
+        {
+            return _HeadingDal.List();
+        }
+
+        public void HeadingAdd(Heading heading)
+        {
+            _HeadingDal.Insert(heading);
+        }
+
+        public void HeadingDelete(Heading heading)
+        {
+            _HeadingDal.Delete(heading);
+        }
+
+        public void HeadingUpdate(Heading heading)
+        {
+            _HeadingDal.Update(heading);
+        }
+    }
+}
