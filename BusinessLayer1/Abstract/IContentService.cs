48921b2f0abcdbf4a5026a4c206023e002ef1213
EsraNurPalut <nuresrapalut@outlook.com>
2022-11-10 18:12:09 +0300

    Content Controller düzenlendi.

---
+++ BusinessLayer1/Abstract/IContentService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer1.Concrete;
+
+namespace BusinessLayer1.Abstract
+{
+     public interface IContentService
+    {
+        List<Content> GetList();
+        List<Content> GetListByID(int id); //ID ye göre bana listeyi döndür
+        void ContentAdd(Content content); //ekle
+        Content GetByID(int id);
+        void ContentDelete(Content content);
+        void ContentUpdate(Content content);
+    }
+}
---
+++ BusinessLayer1/Concrete/ContentManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer1.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer1.Concrete;
+
+namespace BusinessLayer1.Concrete
+{
+    public class ContentManager : IContentService
+    {
+
+        IContentDal _contentDal;
+
+        public ContentManager(IContentDal contentDal)
+        {
+            _contentDal = contentDal;
+        }
+
+        public void ContentAdd(Content content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ContentDelete(Content content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ContentUpdate(Content content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Content GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Content> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Content> GetListByID(int id)
+        {
+            return _contentDal.List(x => x.HeadingID == id); //dısarıdan girdiğim id ye göre listeleme yapacak
+        }
+    }
+}
