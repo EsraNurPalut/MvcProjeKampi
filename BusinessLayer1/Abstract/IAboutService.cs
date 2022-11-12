8e9ff1bfddced6ea7aec1b8180b5a36d4f5e3654
EsraNurPalut <nuresrapalut@outlook.com>
2022-11-12 22:17:24 +0300

    popup kullanımı

---
+++ BusinessLayer1/Abstract/IAboutService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer1.Concrete;
+
+namespace BusinessLayer1.Abstract
+{
+   public interface IAboutService
+    {
+
+        List<About> GetList();
+        void AboutAdd(About about); //ekle
+        About GetByID(int id);
+        void AboutDelete(About about);
+        void AboutUpdate(About about);
+    }
+}
---
+++ BusinessLayer1/Concrete/AboutManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer1.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer1.Concrete;
+
+namespace BusinessLayer1.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        IAboutDal _aboutDal;
+
+        public AboutManager(IAboutDal aboutDal) //constr metor
+        {
+            _aboutDal = aboutDal;
+        }
+
+        public void AboutAdd(About about)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AboutDelete(About about)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AboutUpdate(About about)
+        {
+            throw new NotImplementedException();
+        }
+
+        public About GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<About> GetList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
