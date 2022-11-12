ca4b3104aae32f281c210b5bef46ccee94b79fe3
EsraNurPalut <nuresrapalut@outlook.com>
2022-11-12 22:51:59 +0300

    About sayfası düzenlendi

--- MvcProjeKampi/Controllers/AboutController.cs
+++ MvcProjeKampi/Controllers/AboutController.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BusinessLayer1.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer1.Concrete;
 
 namespace MvcProjeKampi.Controllers
 {
     public class AboutController : Controller
     {
-        // GET: About
+        AboutManager abm = new AboutManager(new EFAboutDal());
+
         public ActionResult Index()
         {
+            var aboutvalues = abm.GetList();
+            return View(aboutvalues);
+        }
+        [HttpGet]
+        public ActionResult AddAbout()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddAbout(About p)
+        {
+            abm.AboutAdd(p);
+            return RedirectToAction("Index");
         }
     }
 }
