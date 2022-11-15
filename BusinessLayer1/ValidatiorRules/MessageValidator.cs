07fc356dff4510920b90c2646a3b30fe8e0c909f
EsraNurPalut <nuresrapalut@outlook.com>
2022-11-15 23:58:59 +0300

    messagevalidator olusturuldu

---
+++ BusinessLayer1/ValidatiorRules/MessageValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer1.Concrete;
+using FluentValidation;
+
+namespace BusinessLayer1.ValidatiorRules
+{
+  public  class MessageValidator:AbstractValidator<Message>
+    {
+        public MessageValidator()
+        {
+            RuleFor(x => x.ReceiverMail).NotEmpty().WithMessage("Alıcı adresini boş gecemezsiniz..");
+
+            RuleFor(x => x.Subject).NotEmpty().WithMessage("Konuyu boş gecemezsiniz..");
+
+            RuleFor(x => x.ReceiverMail).NotEmpty().WithMessage("Alıcı adresini boş gecemezsiniz..");
+        }
+    }
+}
