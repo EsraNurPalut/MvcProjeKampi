732a7e8652f916ca9f357be90891dfd62c92ed14
EsraNurPalut <nuresrapalut@outlook.com>
2022-11-17 22:15:11 +0300

    Gallery -ImageFile Add

---
+++ DataAccessLayer/EntityFramework/EFImageFileDal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.Repositories;
+using EntityLayer1;
+
+namespace DataAccessLayer.EntityFramework
+{
+    class EFImageFileDal:GenericRepository<İmageFile>,IImageFileDal
+    {
+    }
+}
