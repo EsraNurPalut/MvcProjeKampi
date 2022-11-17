d705641c94389e70cf37664d222d99d99dde3853
EsraNurPalut <nuresrapalut@outlook.com>
2022-11-17 22:19:24 +0300

    ImageFile Edit

---
+++ BusinessLayer1/Concrete/ImageFileManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer1.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer1;
+
+namespace BusinessLayer1.Concrete
+{
+    public class ImageFileManager : IImageFileService
+    {
+        IImageFileDal _imagefileDal;
+
+        public ImageFileManager(IImageFileDal imagefileDal)
+        {
+            _imagefileDal = imagefileDal;
+        }
+
+        public List<İmageFile> GetList()
+        {
+           return  _imagefileDal.List();
+        }
+    }
+}
