d8ab885f120fe1c92acb41484ff2f244d5c1146c
EsraNurPalut <nuresrapalut@outlook.com>
2022-11-19 15:48:56 +0300

    Login Edit

--- MvcProjeKampi/Controllers/AdminCategoryController.cs
+++ MvcProjeKampi/Controllers/AdminCategoryController.cs
@@ -15,6 +15,7 @@ namespace MvcProjeKampi.Controllers
     {
         CategoryManager cm = new CategoryManager(new EFCategoryDal());
 
+        [Authorize]
         public ActionResult Index()
         {
             var categoryvalues = cm.GetList();
---
+++ MvcProjeKampi/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataAccessLayer.Concrete;
+using EntityLayer1.Concrete;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Admin p)
+        {
+            Context c = new Context();
+            var adminuserinfo = c.Admins.FirstOrDefault(x => x.AdminUserName == p.AdminUserName && x.AdminPassword == p.AdminPassword);
+            if(adminuserinfo!=null) 
+            {
+                return RedirectToAction("Index", "AdminCategory");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+               
+
+            }
+
+            return View();
+        }
+    }
+}
