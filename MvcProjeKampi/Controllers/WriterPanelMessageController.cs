fc777330d6d573693c9d3d2bac7af1aa9719f09e
EsraNurPalut <nuresrapalut@outlook.com>
2022-11-21 22:53:49 +0300

    writerpanelmessage edit

--- MvcProjeKampi/Controllers/WriterPanelMessageController.cs
+++ MvcProjeKampi/Controllers/WriterPanelMessageController.cs
@@ -6,6 +6,8 @@ using System.Web.Mvc;
 using BusinessLayer1.Concrete;
 using BusinessLayer1.ValidatiorRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer1.Concrete;
+using FluentValidation.Results;
 
 namespace MvcProjeKampi.Controllers
 {
@@ -47,6 +49,31 @@ namespace MvcProjeKampi.Controllers
         {
             var values = mm.GetByID(id);
             return View(values);
+        }
+
+
+        [HttpGet]
+        public ActionResult NewMessage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult NewMessage(Message p)
+        {
+            ValidationResult result = messagevalidator.Validate(p);
+            if (result.IsValid)  //sonuclar dogru ise
+            {
+                p.MessageDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                mm.MessageAdd(p);
+                return RedirectToAction("Sendbox");
+            }
+            else {
+                foreach (var item in result.Errors) {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
         }
 
     }
