6ce6784602ffea1a5925d343d0c4090c8f9e9ba7
EsraNurPalut <nuresrapalut@outlook.com>
2022-11-21 22:43:40 +0300

    WriterMessage Edit

---
+++ MvcProjeKampi/Controllers/WriterPanelMessageController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer1.Concrete;
+using BusinessLayer1.ValidatiorRules;
+using DataAccessLayer.EntityFramework;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class WriterPanelMessageController : Controller
+    {
+        // GET: WriterPanelMessage
+
+        MessageManager mm = new MessageManager(new EFMessageDal());
+
+        MessageValidator messagevalidator = new MessageValidator();
+
+
+        public ActionResult Inbox()
+        {
+            var messagelist = mm.GetListInbox();
+            return View(messagelist);
+        }
+
+        public ActionResult Sendbox()
+        {
+            var messagelist = mm.GetListSendbox();
+            return View(messagelist);
+        }
+
+        public ActionResult MessageListMenu()
+        {
+
+            return PartialView();
+        }
+
+
+        public ActionResult GetInboxMessageDetails(int id)
+        {
+            var values = mm.GetByID(id);
+            return View(values);
+        }
+
+        public ActionResult GetSendboxMessageDetails(int id)
+        {
+            var values = mm.GetByID(id);
+            return View(values);
+        }
+
+    }
+}
