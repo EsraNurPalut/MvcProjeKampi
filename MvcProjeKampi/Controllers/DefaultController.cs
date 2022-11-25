6c06efa3b314ba64cd75700f3e5b7e4a67d2dc4b
EsraNurPalut <nuresrapalut@outlook.com>
2022-11-25 19:46:42 +0300

    Headings Edit.

--- MvcProjeKampi/Controllers/DefaultController.cs
+++ MvcProjeKampi/Controllers/DefaultController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BusinessLayer1.Concrete;
+using DataAccessLayer.EntityFramework;
 
 namespace MvcProjeKampi.Controllers
 {
     public class DefaultController : Controller
     {
         // GET: Default
+        HeadingManager hm = new HeadingManager(new EFHeadingDal());
         public ActionResult Index()
         {
             return View();
